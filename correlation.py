b135a25a7db97064bfbb4eca811ca25e1c25a337
samuel <samuel86102@gmail.com>
2022-01-09 16:03:03 +0800

    change commit

--- correlation.py
+++ correlation.py
@@ -3,14 +3,13 @@ import seaborn as sn
 import matplotlib.pyplot as plt
 from matplotlib import rcParams
 rcParams.update({'figure.autolayout': True})
-
+pd.set_option("display.max_rows", None, "display.max_columns", None)
 df = pd.read_csv('input/train.csv')
 
 # Convert string attributes to categorical attributes
 df['Primary'] = df['Primary'].astype('category').cat.codes
 df['Secondary'] = df['Secondary'].astype('category').cat.codes
 df['Legendary'] = df['Legendary'].astype('category').cat.codes
-
 print(df.info())
 
 # Correlation Matrix
