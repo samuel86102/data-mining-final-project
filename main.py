9b542dca2d9ea9a66e7cec0bb2010ae655628452
samuel <samuel86102@gmail.com>
2022-01-17 18:42:43 +0800

    change

--- main.py
+++ main.py
@@ -7,10 +7,16 @@ import matplotlib.pyplot as plt
 df = pd.read_csv('input/train.csv')
 
 features = ['Attack','Defense','Escape_rate','MaxCP']
+
+print(df['Capture_rate'].describe())
+discretize(df)
+print(df['Capture_rate'].describe())
+
+
+
 X = df[features]
 
 y = df['Capture_rate']
 
-discretize(y)
 
 
--- mylib.py
+++ mylib.py
@@ -1,5 +1,15 @@
 import pandas as pd
 
-def discretize(s):
-    print(s)
+def f(x):
+    cr = x['Capture_rate']
+    if 0 <= cr <= 0.25: return 0
+    elif 0.25 < cr <= 0.5: return 1
+    elif 0.5 < cr <= 0.75: return 2
+    elif 0.75 < cr <= 1: return 3
+    else: return None
+
+
+def discretize(df):
     
+    df['Capture_rate'] = df.apply(f, axis=1)
+
